45c470b32edcc4cca8d47573dc6c2c2e906746f3
nqa2310 <ailavip1st@gmail.com>
2020-07-15 19:55:48 +0700

    ss18 on class

--- session_17/src/components/parts/TableGroup/index.js
+++ session_17/src/components/parts/TableGroup/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import CDropdown from '../../pieces/Dropdown'
 import CTable from '../../pieces/Table'
 import SearchForm from './SearchForm'
@@ -7,6 +9,42 @@ import TableControls from './TableControls'
 class TableGroup extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      list: [], // Table
+
+      pageIndex: 1,
+      pageSize: 10, // TableControls
+      search: '', // SearchForm
+      filter: {
+        categoryId: null // Dropdown
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.callApi()
+  }
+
+  callApi() {
+    let { pageIndex, pageSize, search, filter } = this.state
+    let queryParams = {
+      pageIndex,
+      pageSize,
+      search,
+      categoryId: filter.categoryId || ''
+    }
+    
+    axios.request({
+      url: 'http://localhost:9000/api/product',
+      method: 'GET',
+      params: queryParams,
+    })
+    .then(res => {
+      console.log('response', res)
+      let list = res.data
+      this.setState({ list })
+    })
   }
 
   render() {
